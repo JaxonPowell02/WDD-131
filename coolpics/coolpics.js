9dc0687a45c4b25a4f601d050381f157ca15ead2
jaxonpowell02 <pow21019@byui.edu>
2024-10-03 14:17:39 -0600

    added the cool pics project

---
+++ coolpics/coolpics.js
@@ -0,0 +1,5 @@
+// coolpics.js
+document.querySelector('.menu-button').addEventListener('click', function() {
+    const nav = document.querySelector('nav');
+    nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+});
